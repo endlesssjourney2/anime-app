60aa923c4f0a08da6446016d663105d4a20c17fd
endlesssjourney2 <endlessj297@gmail.com>
2026-08-25 23:44:55 +0200

    feat: add anime details route

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./Layout/Layout";
 import Main from "./pages/Main/Main";
+import AnimeDetails from "./pages/Title/AnimeDetais";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ index: true, element: <Main /> }],
+    children: [
+      { index: true, element: <Main /> },
+      { path: "anime/:id", element: <AnimeDetails /> },
+    ],
   },
 ]);

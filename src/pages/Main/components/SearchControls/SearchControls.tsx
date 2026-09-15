dfea21d8d072244e498b3490b7fb332c8dad72ac
endlesssjourney2 <endlessj297@gmail.com>
2026-09-15 23:18:01 +0200

    feat: add anime search controls component

---
+++ src/pages/Main/components/SearchControls/SearchControls.tsx
@@ -0,0 +1,45 @@
+import type { FC } from "react";
+import s from "./SearchAnime.module.css";
+import type { AniListSort } from "../../../../types/AniListSort";
+
+type Props = {
+  searchItem: string;
+  setSearchItem: (v: string) => void;
+  sort: AniListSort;
+  setSort: (v: AniListSort) => void;
+  loading: boolean;
+};
+
+const SearchControls: FC<Props> = ({
+  searchItem,
+  setSearchItem,
+  sort,
+  setSort,
+  loading,
+}) => {
+  return (
+    <div className={s.searchContainer}>
+      <input
+        disabled={loading}
+        className={s.searchInput}
+        type="text"
+        value={searchItem}
+        onChange={(e) => setSearchItem(e.target.value)}
+        placeholder="Search anime..."
+      />
+
+      <select
+        disabled={loading}
+        value={sort}
+        onChange={(e) => setSort(e.target.value as AniListSort)}
+        className={s.sortSelect}
+      >
+        <option value="SCORE_DESC">Score</option>
+        <option value="POPULARITY_DESC">Popularity</option>
+        <option value="TRENDING_DESC">Trending</option>
+      </select>
+    </div>
+  );
+};
+
+export default SearchControls;

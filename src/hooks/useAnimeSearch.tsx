262a287017938f41d43853d0a4ea3a44b411dd1e
endlesssjourney2 <endlessj297@gmail.com>
2026-09-15 23:15:16 +0200

    feat: add sorting to anime search hook

--- src/hooks/useAnimeSearch.tsx
+++ src/hooks/useAnimeSearch.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { searchAnime } from "../api/aniListApi";
 import useDebounce from "./useDebounce";
 import type { AniListMedia, AniListPageInfo } from "../types/AniList";
+import type { AniListSort } from "../types/AniListSort";
 
 const useAnimeSearch = (perPage: number) => {
   const [searchItem, setSearchItem] = useState("");
@@ -9,12 +10,13 @@ const useAnimeSearch = (perPage: number) => {
   const [pageInfo, setPageInfo] = useState<AniListPageInfo | null>(null);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [sort, setSort] = useState<AniListSort>("SCORE_DESC");
 
   const debouncedSearchItem = useDebounce(searchItem, 500);
 
   useEffect(() => {
     setLoading(true);
-    searchAnime(debouncedSearchItem, page, perPage)
+    searchAnime(debouncedSearchItem, page, perPage, [sort])
       .then((result) => {
         setResults(result.media);
         setPageInfo(result.pageInfo);
@@ -25,11 +27,11 @@ const useAnimeSearch = (perPage: number) => {
       .finally(() => {
         setLoading(false);
       });
-  }, [debouncedSearchItem, page]);
+  }, [debouncedSearchItem, page, sort]);
 
   useEffect(() => {
     setPage(1);
-  }, [debouncedSearchItem]);
+  }, [debouncedSearchItem, sort]);
 
   return {
     searchItem,
@@ -39,6 +41,8 @@ const useAnimeSearch = (perPage: number) => {
     page,
     setPage,
     loading,
+    sort,
+    setSort,
   };
 };
 

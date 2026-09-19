0cbf9841bc31355a8873aca407b78272efb3b540
endlesssjourney2 <endlessj297@gmail.com>
2026-09-20 01:00:28 +0200

    feat: redesign character cards with role and profile details(age, birthday, gender)

--- src/pages/Title/components/CharactersSection/components/CharacterCard/CharacterCard.tsx
+++ src/pages/Title/components/CharactersSection/components/CharacterCard/CharacterCard.tsx
@@ -1,20 +1,66 @@
-import type { CharacterRole } from "../../../../../../types/Character";
+import {
+  IconGenderFemale,
+  IconGenderMale,
+  IconGift,
+  IconHourglass,
+} from "@tabler/icons-react";
+import { checkBirthday } from "../../../../../../helpers/checkBirthday";
+import type {
+  CharacterRole,
+  DateOfBirth,
+} from "../../../../../../types/Character";
 import s from "./CharacterCard.module.css";
 import type { FC } from "react";
 
 type Props = {
   image: string;
-  name: string;
+  name: { full: string; native: string };
   role: CharacterRole;
+  dateOfBirth: DateOfBirth;
+  age: string;
+  gender: string;
 };
 
-const CharacterCard: FC<Props> = ({ role, name, image }) => {
+const CharacterCard: FC<Props> = ({
+  role,
+  name,
+  image,
+  dateOfBirth,
+  age,
+  gender,
+}) => {
+  const GenderIcon =
+    gender === "Male"
+      ? IconGenderMale
+      : gender === "Female"
+        ? IconGenderFemale
+        : null;
+
   return (
     <div className={s.item}>
-      <img className={s.image} src={image} alt={name} />
+      <img className={s.image} src={image} alt={name.native} />
       <div className={s.info}>
-        <p className={s.name}>{name}</p>
-        <p className={`${s.role} ${s[role]}`}>{role}</p>
+        <div className={s.header}>
+          <span className={s.nameFull}>{name.full}</span>
+          <span className={s.nameNative}>{name.native}</span>
+        </div>
+        <div className={s.mid}>
+          <span className={`${s.role} ${s[role]}`}>{role}</span>
+        </div>
+        <div className={s.meta}>
+          <span className={`${s.metaItem} ${s.age}`}>
+            <IconHourglass size={13} />
+            {age ?? "Unknown"}
+          </span>
+          <span className={`${s.metaItem} ${s.birthday}`}>
+            <IconGift size={13} />
+            {checkBirthday(dateOfBirth.day, dateOfBirth.month)}
+          </span>
+          <span className={`${s.metaItem} ${s.gender}`}>
+            <GenderIcon size={13} />
+            {gender}
+          </span>
+        </div>
       </div>
     </div>
   );

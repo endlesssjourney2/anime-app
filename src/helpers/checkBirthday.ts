865660926a87dacef331dc52475940d85121cc86
endlesssjourney2 <endlessj297@gmail.com>
2026-09-20 00:57:05 +0200

    feat: add character birthday formatter helper

---
+++ src/helpers/checkBirthday.ts
@@ -0,0 +1,9 @@
+import dayjs from "dayjs";
+
+export const checkBirthday = (day: number | null, month: number | null) => {
+  if (!day || !month) return "Unknown";
+
+  return dayjs(day)
+    .month(month - 1)
+    .format("MMMM D");
+};

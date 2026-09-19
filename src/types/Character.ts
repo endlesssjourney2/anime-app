d88a925600654580408f2c697917e6a393a6b541
endlesssjourney2 <endlessj297@gmail.com>
2026-09-20 00:55:21 +0200

    feat: add character age, gender and birth date types

--- src/types/Character.ts
+++ src/types/Character.ts
@@ -1,7 +1,15 @@
 type CharacterNode = {
   id: number;
-  name: { full: string };
+  name: { full: string | null; native: string | null };
   image: { large: string };
+  dateOfBirth: DateOfBirth;
+  age: string;
+  gender: string;
+};
+
+export type DateOfBirth = {
+  month: number;
+  day: number;
 };
 
 export type CharacterRole = "MAIN" | "SUPPORTING" | "BACKGROUND";
